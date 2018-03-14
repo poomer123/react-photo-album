106179939c264eda978110d8f75c2efe506096b4
ApiratChiwakanit <apirat.c@roundtwosolutions.com>
2018-03-14 13:52:23 +0700

    พื้นฐาน Redux และการ Config เบื้องต้น

--- src/index.js
+++ src/index.js
@@ -4,6 +4,25 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import {BrowserRouter as Router} from 'react-router-dom'
+import { createStore } from 'redux'
+
+
+function countAge(state = 0, action) {
+    switch (action.type) {
+        case "UPAGE":
+            return state + 1
+        case "DOWNAGE":
+            return state - 1
+        default:
+            return state
+    }
+}
+
+const store = createStore(countAge)
+store.subscribe(()=>{console.warn('subscibe', store.getState())})
+store.dispatch({
+    type: "UPAGE"
+})
 
 ReactDOM.render(
     <Router>
