429235da2245856134a06687d9c54229749875ec
ApiratChiwakanit <apirat.c@roundtwosolutions.com>
2018-03-14 13:33:05 +0700

    ปัญหาของ State โดยไม่ใช้ Redux

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
 
 class Home extends Component {
+  state = { age: 0}
   render() {
     return (
       <div>
-        <h1>Home</h1>
+        <h1>อายุ: {this.state.age} ปี</h1>
+        <button onClick={this.upAge}>+ อายุ</button>
+        <button onClick={this.downAge}>- อายุ</button>
       </div>
     )
+  }
+
+
+  upAge = () => {
+    this.setState((ps) => {
+        return {
+            age: ps.age + 1 
+            
+        }
+    })
+  }
+
+  downAge = () => {
+    this.setState((ps) => {
+        return {
+            age: ps.age - 1
+        }
+    })
   }
 }
 
