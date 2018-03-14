6ee4ed1af603a9a5bd036a9803c2626c49fe4451
ApiratChiwakanit <apirat.c@roundtwosolutions.com>
2018-03-14 16:16:10 +0700

    สร้างหน้าจอแสดงรายชื่อ USER

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,13 +1,45 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import UserList from '../components/users/UserList'
 
 class User extends Component {
+  state = { data: null, err: null }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => {
+          if( res.ok ) {
+            return res
+          }
+          throw Error(res.status)
+      })
+      .then(rs => rs.json())
+      .then(rs => this.setState({ data: rs }))
+      .catch( e => this.setState({ err: e.message }))
+  }
+
   render() {
+    const { data, err } = this.state
+    let list = <div>Loading...</div>
+
+    if(data) {
+      if(data.length > 0) {
+        list = <UserList data={data} />
+      } else {
+        list = <h4>No Data</h4>
+      }
+    }
+
+    if(err) {
+      list = <h4>เกิดข้อผิดพลาด : {err}</h4>
+    }
+
     return (
       <div>
         <h1>User</h1>
+        {list}
       </div>
     )
   }
 }
 
-export default User;
+export default User
