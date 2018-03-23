4ab21c02d2502ca5ef173583489140f1fc48e1f9
ApiratChiwakanit <apirat.c@roundtwosolutions.com>
2018-03-23 13:35:47 +0700

    PageNotFound โดยใช้ Switch ใน ReactRouter v.4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import Header from './components/layouts/Header'
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 
 import Home from './pages/Home'
 import User from './pages/User'
 import Album from './pages/Album'
 import Photo from './pages/Photo'
+import NotFound from './pages/NotFound'
 
 class App extends Component {
   render() {
@@ -14,10 +15,13 @@ class App extends Component {
         <Header />
 
         <div className="container">
-          <Route exact path="/" component={Home} />
-          <Route path="/user" component={User} />
-          <Route path="/album/:id" component={Album} />
-          <Route path="/photo/:id" component={Photo} />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/user" component={User} />
+            <Route path="/album/:id" component={Album} />
+            <Route path="/photo/:id" component={Photo} />
+            <Route component={NotFound} />
+          </Switch>
         </div>
 
       </div>
