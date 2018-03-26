d17983d4c4d72c0c15b3990800b44d25738c408d
ApiratChiwakanit <apirat.c@roundtwosolutions.com>
2018-03-26 10:51:39 +0700

    ปรับ Header ไปใช้ Navbar ของ reactstrap

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 import { withRouter, Link } from 'react-router-dom'
+import { Collapse, Navbar, NavbarToggler, NavbarBrand } from 'reactstrap';
 
 class Header extends Component {
+  state = {
+    isOpen: true
+  }
+  toggleNavbar = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    })
+  }
   render() {
     return (
       <div>
-        <h1>Header</h1>
-        {this.renderLinks()}
+        <Navbar color="faded" light expand="md">
+          <NavbarBrand className="mr-auto">Header</NavbarBrand>
+          <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+          <Collapse isOpen={!this.state.isOpen} navbar>
+            {this.renderLinks()}
+          </Collapse>
+        </Navbar>
       </div>
     );
   }
@@ -14,10 +28,10 @@ class Header extends Component {
   renderLinks = () => {
     if (localStorage.getItem('access-token')) {
       return (
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/user">Photo</Link></li>
-          <li><a href="" onClick={this.signOut}>ออกจากระบบ</a></li>
+        <ul className="navbar-nav">
+          <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
+          <li className="nav-item"><Link className="nav-link" to="/user">Photo</Link></li>
+          <li className="nav-item"><a className="nav-link" href="" onClick={this.signOut}>ออกจากระบบ</a></li>
         </ul>
       )
     }
